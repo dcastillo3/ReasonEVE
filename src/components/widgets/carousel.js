d281c52de4aa42a10e11e20e3884b5b031ead267
Daniel <dcastillo3@gmail.com>
2019-04-08 17:15:05 -0400

    Feat(carousel.js): Create carousel widget.

---
+++ src/components/widgets/carousel.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Carousel as CarouselWidget } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const Carousel = props => {
+
+    return (
+        <div id="carousel-widget" className="carousel-widget flex-column flex-center palette-three bottom-blue">
+            <div className="carousel-content flex-column flex-center narrow-content large-padding">
+
+                <CarouselWidget
+                    showStatus={false}
+                    showIndicators={false}
+                    showThumbs={false}
+                    infiniteLoop={true}
+                    autoPlay={true}
+                    interval={10000}
+                    transitionTime={1500}
+                >
+                {/* Array of items */}
+                </CarouselWidget>
+
+            </div>
+        </div>
+    )
+}
+
+export default Carousel;
