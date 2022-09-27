02e526207757eb974db53ae5a2c30801ce72a87d
Daniel <dcastillo3@gmail.com>
2022-09-27 16:16:16 -0700

    feature(audioPlayer.js): Add audio player to repo

---
+++ src/components/audioPlayer/audioPlayer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MusicPlayer from 'react-material-music-player';
+
+function AudioPlayer() {
+    return (
+        <div>
+            <MusicPlayer sx={{}} disableDrawer={false} />
+        </div>
+    );
+};
+
+export default AudioPlayer;
