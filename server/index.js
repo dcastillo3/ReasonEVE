09e0b1714a55d1d796b3f3a23109d1955336b807
Daniel <dcastillo3@gmail.com>
2022-09-28 01:17:49 -0700

    chore(index.js): Add express and react proxy to application. Served mp3 file via express static route

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+// const bodyParser = require('body-parser');
+
+const server = express();
+const port = 8000;
+
+// server.use(bodyParser.urlencoded({ extended: true }));
+// server.use(bodyParser.json());
+
+server.use(morgan('dev'));
+
+server.use('/api/beats', express.static('beats'));
+
+server.use('/api', require('./api'));
+
+server.listen(port, function () {
+    console.log('Server is running on port: ', port);
+});
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { render } from 'react-dom';
 import { Track, PlayerInterface } from 'react-material-music-player';
 import { AudioPlayer } from './components/audioPlayer';
@@ -9,20 +10,40 @@ const beat1 = new Track(
     'https://media.gettyimages.com/photos/line-subway-train-in-queens-with-manhattan-skyline-new-york-city-picture-id1189547726?s=612x612', //HTML <img> tag
     'Another Evening',
     'ReasonEVE',
-    '../beats/Another%20Evening.mp3'
+    '../api/beats/Another Evening.mp3'
 );
 
 // Create playlist
 const myPlaylist = [
     beat1
-]
+];
 
 // Add playlist to player
 PlayerInterface.play(myPlaylist);
 
+// Test axios api call
+function ExpressTestComponent() {
+    const [expressData, setExpressData] = useState('');
+    const textExpressCall = async () => {
+        const res = await axios.get('/api/testForReact');
+        console.log(res);
+
+        setExpressData(res.data);
+    };
+
+    useEffect(() => {
+        textExpressCall();
+    }, []);
+
+    return(
+        <div>{expressData ? expressData : 'no express data found'}</div>
+    );
+}
+
 render(
     <div>
         <div>index.js</div>
+        <ExpressTestComponent />
         <AudioPlayer />
     </div>,
     document.getElementById('root')
