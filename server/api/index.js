d03e5ae8c57fb00b59d7b2f4466b323a8f5693c6
Daniel <dcastillo3@gmail.com>
2022-09-29 02:31:55 -0700

    feature(app.js): Read tracks in tracks directory and generate playlist with jsdelivr CDN url

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,30 @@
 const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
 
-router.get('/testForReact', (req, res) => {
-    console.log('I am in the test express route')
-    res.send('This message is from api/testForReact!')
+// Read tracks directory and generate track data
+router.get('/getTracks', (req, res) => {
+    const tracksDirectory = path.join(__dirname, '../../tracks');
+
+    fs.readdir(tracksDirectory, (err, tracks) => {
+        if (err) {
+            console.log('Make sure tracks directory is in root');
+            throw err;
+        };
+
+        if (tracks.length) {
+            const cdnBaseUrl = 'https://cdn.jsdelivr.net/gh/dcastillo3/ReasonEVE/tracks/';
+            const playlist = tracks.map((track, id) => ({
+                id,
+                coverArt: 'https://media.gettyimages.com/photos/line-subway-train-in-queens-with-manhattan-skyline-new-york-city-picture-id1189547726?s=612x612',
+                title: path.parse(track).name,
+                artist: 'ReasonEVE',
+                url: `${cdnBaseUrl}/${track}`
+            }));
+
+            res.send(playlist);
+        }
+    });
 });
 
 router.use((req, res, next) => {
