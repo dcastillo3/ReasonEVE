4d57c0f5cc5b238710b45f103346309c8453eab8
Daniel <dcastillo3@gmail.com>
2023-04-15 06:34:34 -0700

    chore(cartLineItemRemoveButton.js): Replaced remove cart item icon

--- src/components/main/cart/components/cartLineItemRemoveButton.js
+++ src/components/main/cart/components/cartLineItemRemoveButton.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Button } from "../../../styled";
 import { CartLineItemRemoveButtonContainer, CartLineItemRemoveIcon } from "../cartStyledComponents";
-import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
+import NotInterestedIcon from '@mui/icons-material/NotInterested';
 
 function CartLineItemRemoveButton({cartItem, removeCartItem}) {
     return (
         <CartLineItemRemoveButtonContainer>
             <Button size={'small'} variant={'warning'} onClick={() => removeCartItem(cartItem)}>
-                <CartLineItemRemoveIcon component={RemoveShoppingCartIcon} />
+                <CartLineItemRemoveIcon component={NotInterestedIcon} />
             </Button>
         </CartLineItemRemoveButtonContainer>
     );
